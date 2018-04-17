aec744b3535fa97bcd218d7866e9a2afac6f2240
Polina <polina@Polinas-Mac-mini.local>
2018-04-17 15:35:14 +0300

    delete comments

--- src/app/forms-page/form.module.ts
+++ src/app/forms-page/form.module.ts
@@ -2,40 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-// import {CdkTableModule} from '@angular/cdk/table';
 import {
-  // MatAutocompleteModule,
-  // MatButtonModule,
-  // MatButtonToggleModule,
-  // MatCardModule,
-  // MatCheckboxModule,
-  // MatChipsModule,
-  // MatStepperModule,
   MatDatepickerModule,
-  // MatDialogModule,
-  // MatDividerModule,
-  // MatExpansionModule,
-  // MatGridListModule,
   MatIconModule,
   MatInputModule,
-  // MatListModule,
-  // MatMenuModule,
   MatNativeDateModule,
-  // MatPaginatorModule,
-  // MatProgressBarModule,
-  // MatProgressSpinnerModule,
   MatRadioModule,
-  // MatRippleModule,
   MatSelectModule,
-  // MatSidenavModule,
-  // MatSliderModule,
-  // MatSlideToggleModule,
-  // MatSnackBarModule,
-  // MatSortModule,
-  // MatTableModule,
-  // MatTabsModule,
-  // MatToolbarModule,
-  // MatTooltipModule
 } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
